3e4774c9b0d0eb18d421621d40a9cba46a09891a
Abdu Assabri <abdusabri@outlook.com>
2018-12-12 19:34:46 +0100

    Updates

---
+++ precache-manifest.cf10511448cf0422bdabbc60bf25adf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41a720648b697401f465",
+    "url": "/fcc-random-quote-generator/static/js/runtime~main.41a72064.js"
+  },
+  {
+    "revision": "b3401e2cd473e37347e2",
+    "url": "/fcc-random-quote-generator/static/js/main.b3401e2c.chunk.js"
+  },
+  {
+    "revision": "e91c12253129cc262ab3",
+    "url": "/fcc-random-quote-generator/static/js/1.e91c1225.chunk.js"
+  },
+  {
+    "revision": "b3401e2cd473e37347e2",
+    "url": "/fcc-random-quote-generator/static/css/main.d2343207.chunk.css"
+  },
+  {
+    "revision": "bc4ff786a6dd45f542b54c1b7c511096",
+    "url": "/fcc-random-quote-generator/index.html"
+  }
+];
